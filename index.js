94d6a23283f51ff0efca18186f4c65593cba7e88
sonozyurt <108385803+sonozyurt@users.noreply.github.com>
2023-08-13 14:10:27 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,50 +1,6 @@
-// var colorSequence = [];
-// var colorsChosenByPlayer = [];
-// var level = 1;
-
-// function createRandomNumber() {
-//   return Math.floor(Math.random() * 4);
-// }
-
-// function newColor() {
-//   var colors = ["red", "green", "yellow", "blue"];
-//   $("#level-title").text(`Level ${level}`);
-//   const randomNumber = createRandomNumber();
-//   colorSequence.push(colors[randomNumber]);
-//   $(`.${colors[randomNumber]}`).fadeOut(200).fadeIn(200);
-//   return colors[randomNumber];
-// }
-
-// $(".btn").click(function (event) {
-//   var currentColor = newColor();
-//   colorsChosenByPlayer.push(event.target.id);
-//   playSound(event.target.id);
-//   pressAnimation(event.target.id);
-// });
-
-// $(document).keydown(function () {
-//   if ($("#level-title").text() === "Press A Key to Start") {
-//     var color = newColor();
-//     playSound(color);
-//     $("#level-title").text(`Level ${level}`);
-//   }
-// });
-
-// function playSound(name) {
-//   var audio = new Audio(`sounds/${name}.mp3`);
-//   audio.play();
-// }
-
-// function pressAnimation(name) {
-//   $(`.${name}`).toggleClass("pressed");
-//   setTimeout(() => {
-//     $(`.${name}`).toggleClass("pressed");
-//   }, 100);
-// }
-
 const colorSequence = [];
-var clickedButtonSeq = [];
-
+const clickedButtonSeq = [];
+var level = 1;
 function randomColor() {
   var colors = ["green", "red", "yellow", "blue"];
   randomNumber = Math.floor(Math.random() * 4);
@@ -62,17 +18,23 @@ $(document).keydown(function () {
 });
 
 $(".btn").click(function (event) {
-  var level = 1;
+  if (
+    $("#level-title").text() !== "Game Over Refresh The Page" &&
+    $("#level-title").text() !== "Press A Key to Start"
+  ) {
+    var level = 1;
+    var clickedButton = event.target.id;
+    clickedButtonSeq.push(clickedButton);
+    if (clickedButtonSeq.length === colorSequence.length) {
+      for (let i = 0; i < colorSequence.length; i++) {
+        if (colorSequence[i] !== clickedButtonSeq[i]) {
+          $("#level-title").text("Game Over Refresh The Page");
+        }
+      }
+      var color = randomColor();
+      $(`.${color}`).fadeOut(200).fadeIn(200);
 
-  var clickedButton = event.target.id;
-  clickedButtonSeq.push(clickedButton);
-  for (let i = 0; i < colorSequence.length; i++) {
-    if (colorSequence[i] !== clickedButtonSeq[i]) {
-      $("#level-title").text("Game over");
+      clickedButtonSeq = [];
     }
   }
-  var color = randomColor();
-  $(`.${color}`).fadeOut(200).fadeIn(200);
-  $("#level-title").text(`Level ${level}`);
-  level++;
 });
